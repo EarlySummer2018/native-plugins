eb14be766ae106c9aadbe1003566fc06f1de56dd
EarlySummer2018 <1687049214@qq.com>
2023-08-25 00:52:41 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# native-plugins
-uniapp 版 安卓原生插件，获取手机app列表
+## cx-getAppList 安卓原生插件
+
+本插件为安卓原生插件，实现根据文件类型获取安卓手机已安装且可以打开该文件类型的APP列表并返回
+### 参数说明
+
+`getOpenAppList(options, callbakc, errCallback)：` 该方法接收三个参数。
+- `options：` 参数列表
+	+ `type：` 文件类型，根据类型获取可以打开该类型文件的App。**`必填`**
+	+ `quality：` 压缩质量，0-100，值越大,压缩后图片质量越高,但图片文件也会越大。100表示无损压缩,也就是不做任何压缩,保留图片原始质量。**`必填`**
+	+ `width：` App 图标宽度。**`必填`**
+	+ `height：` App 图标高度。**`必填`**
+- `callbakc：`成功回调，返回App列表，图标为 `base64`	格式，渲染时需要自行添加前缀 `data:image/png;base64,`
+- `errCallback`失败回调，返回错误信息
+
+### 使用方法
+
+##### vue2
+
+```vue
+<template>
+	<view>
+		<button @click="getAppList()"></button>
+	</view>
+</template>
+```
+```js
+export default {
+	// #ifdef APP-PLUS
+	// requireNativePlugin 是 app 端才有的api，最好加上标识，以免报错导致我编译失败
+	const plugins = uni.requireNativePlugin('cx-getAppList')
+	// #endif
+	data(){
+		return {
+			appList: []
+		}
+	}
+	methods:{
+		getAppList() {
+			const that = this
+			plugin.getOpenAppList({
+				type: 'image/*',
+				quality: 100,
+				width: 60,
+				height: 60
+			}, res => {
+				if (!res.length) {
+					uni.showModal({
+						title: '温馨提示',
+						content: '未找到可打开该文件的APP',
+						icon: 'none'
+					})
+				} else {
+					that.appList = res
+				}
+			},
+			err=>{
+				console.log(err)
+			})
+		}
+	}
+}
+```
+
+##### vue3
+
+```vue
+<template>
+	<view>
+		<button @click="getAppList()"></button>
+	</view>
+</template>
+```
+```ts
+<script lang="ts" setup>
+// #ifdef APP-PLUS
+// requireNativePlugin 是 app 端才有的api，最好加上标识，以免报错导致我编译失败
+const plugins = uni.requireNativePlugin('cx-getAppList')
+// #endif
+
+const getAppList = () => {
+	const that = this
+	plugin.getOpenAppList({
+		type: 'image/*',
+		quality: 100,
+		width: 60,
+		height: 60
+	}, res => {
+		if (!res.length) {
+			uni.showModal({
+				title: '温馨提示',
+				content: '未找到可打开该文件的APP',
+				icon: 'none'
+			})
+		} else {
+			that.appList = res
+		}
+	},
+	err=>{
+		console.log(err)
+	})
+}
+</script>
+```
---
+++ open_applist_plugin/src/main/java/com/cx/open_applist_plugin/OpenAppListModule.java
@@ -0,0 +1,105 @@
+package com.cx.open_applist_plugin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+import android.util.Log;
+
+import io.dcloud.feature.uniapp.annotation.UniJSMethod;
+import io.dcloud.feature.uniapp.bridge.UniJSCallback;
+import io.dcloud.feature.uniapp.common.UniModule;
+
+public class OpenAppListModule extends UniModule {
+
+    public String TYPE      = "type";           // 获取的APP类型
+    public String QUALITY   = "quality";        // 压缩质量
+    public String WIDTH     = "width";          // 获取图标指定宽度
+    public String HEIGHT    = "height";         // 获取图标指定高度
+
+    @UniJSMethod(uiThread = true)
+    public void getOpenAppList(JSONObject options, UniJSCallback callback, UniJSCallback errCallback) {
+        try {
+            Log.i("appList", "开始获取");
+            //  如果参数不存在，则设置默认值
+            if (options == null) {
+                options.put("type", "image/*");
+                options.put("quality", 100);
+                options.put("width", 60);
+                options.put("height", 60);
+            }
+            Context context = mUniSDKInstance.getContext();
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setType(options.getString(TYPE));
+
+            PackageManager packageManager = context.getPackageManager();
+            List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(intent, 0);
+
+            JSONArray appList = new JSONArray();
+
+            for (ResolveInfo resolveInfo : resolveInfoList) {
+                JSONObject appInfo = new JSONObject();
+                appInfo.put("name", resolveInfo.activityInfo.applicationInfo.loadLabel(packageManager).toString());
+                appInfo.put("packageName", resolveInfo.activityInfo.packageName);
+
+                Drawable icon = resolveInfo.activityInfo.applicationInfo.loadIcon(packageManager);
+
+                Bitmap bitmap = drawableToBitmap(icon, options.getInteger(WIDTH), options.getInteger(HEIGHT));
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, options.getInteger(QUALITY), baos);
+                byte[] bytes = baos.toByteArray();
+                String iconBase64 = Base64.encodeToString(bytes, Base64.NO_WRAP);
+
+                appInfo.put("icon", iconBase64);
+
+                appList.add(appInfo);
+            }
+            Log.d("appList", appList.toString());
+            Log.i("appList", "获取结束");
+            if (callback != null) {
+                callback.invokeAndKeepAlive(appList);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (errCallback != null) {
+                JSONObject data = new JSONObject();
+                data.put("err", e);
+                errCallback.invoke(data);
+            }
+            throw new RuntimeException(e);
+        }
+    }
+    // drawable转bitmap
+    private Bitmap drawableToBitmap(Drawable drawable, int width, int height) {
+        Bitmap bitmap;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            bitmap = bitmapDrawable.getBitmap();
+        } else {
+//            int width = drawable.getIntrinsicWidth();
+//            int height = drawable.getIntrinsicHeight();
+            bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, width, height);
+            drawable.draw(canvas);
+        }
+
+        return bitmap;
+    }
+}
